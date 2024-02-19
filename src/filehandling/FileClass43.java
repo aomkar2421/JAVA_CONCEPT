cc0d80b2648cc746d589545777e73fefccf9329d
aomkar2421 <omkarjagtap1651@gmail.com>
2024-02-19 10:14:31 +0530

    19 February

--- src/filehandling/FileClass43.java
+++ src/filehandling/FileClass43.java
@@ -1,14 +1,20 @@
 package filehandling;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileClass43 {
-	public static void main(String[] args) {
-		String path1 = "D:/Utilities/Files/abc.txt";
-		String path2 = "abc.txt";
+	public static void main(String[] args) throws IOException {
+		String path1 = "D:/Utilities/Files/omkar.txt";
+		String path2 = "omkar.txt";
+		String path3 = "om.txt";
 
-		File file = new File(path2);
+		File file = new File(path3);
+		file.createNewFile();
 		System.out.println(file.exists());
 		System.out.println(file.getName());
+		
+		File file2 = new File(path3);
+		file2.delete();
 	}
 }
